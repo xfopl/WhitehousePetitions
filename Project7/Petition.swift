ba9c45ccca8237d9cdcfaa84e5b5e433df1f8b52
Aleksandr Veselov <fcaeoff@gmail.com>
2022-09-26 23:08:42 +0500

    Initial Commit

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Александр Веселов on 8/7/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
